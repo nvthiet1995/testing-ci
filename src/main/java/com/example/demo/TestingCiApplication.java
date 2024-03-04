c49a1a2c6243b4db9a062c50963e1a821cddad02
Thiet Nguyen <nvthiet.it.95@gmail.com>
2024-03-04 21:38:36 +0700

    first commit

---
+++ src/main/java/com/example/demo/TestingCiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingCiApplication.class, args);
+	}
+
+}
