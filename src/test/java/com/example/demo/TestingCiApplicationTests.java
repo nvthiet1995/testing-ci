d55cd97b4e825ab4862c8d33fad29b16abf392b9
Thiet Nguyen <nvthiet.it.95@gmail.com>
2024-03-06 22:39:21 +0700

    Add fail test case for testing ci

--- src/test/java/com/example/demo/TestingCiApplicationTests.java
+++ src/test/java/com/example/demo/TestingCiApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TestingCiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 2);
 	}
 
 }
